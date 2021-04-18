fbe99f846109f08b6dc3fa91965604c2188ffcee
Guilherme Pereira de Oliveira <guilhermepdo1508@gmail.com>
2021-04-18 15:41:46 -0300

    Requisito 4

--- script.js
+++ script.js
@@ -2,18 +2,17 @@ const rgbColor = document.querySelector('#rgb-color');
 const colorOptions = document.querySelector('#color-options');
 
 function randomRgb() {
-  let rgb = 'Qual é a cor rgb(';
+  let rgb = 'rgb(';
   let randomNumber = Math.round(Math.random() * 256);
   rgb = rgb.concat(randomNumber, ', ');
   randomNumber = Math.round(Math.random() * 256);
   rgb = rgb.concat(randomNumber, ', ');
   randomNumber = Math.round(Math.random() * 256);
-  rgb = rgb.concat(randomNumber, ')?');
-  console.log(rgb);
+  rgb = rgb.concat(randomNumber, ')');
   return rgb;
 }
 
-function colorOptionsContainer(numberOfOptions) {
+function optionsContainer(numberOfOptions) {
   for (let index = 0; index < numberOfOptions; index += 1) {
     const newColor = document.createElement('div');
     newColor.classList.add('ball');
@@ -21,5 +20,21 @@ function colorOptionsContainer(numberOfOptions) {
   }
 }
 
-rgbColor.innerText = randomRgb();
-colorOptionsContainer(6);
+function randomColors() {
+  const allOptions = document.querySelectorAll('.ball');
+  const rgbBegin = rgbColor.innerText.indexOf('(') - 3;
+  const rgbEnd = rgbColor.innerText.indexOf('?');
+  const rightOption = rgbColor.innerText.substr(rgbBegin, rgbEnd - rgbBegin);
+  const randomPosition = Math.round(Math.random() * allOptions.length);
+  for (let index = 0; index < allOptions.length; index += 1) {
+    if (index === randomPosition) {
+      allOptions[index].style.backgroundColor = rightOption;
+    } else {
+      allOptions[index].style.backgroundColor = randomRgb();
+    }
+  }
+}
+
+rgbColor.innerText = ('Qual é a cor ').concat(randomRgb(), '?');
+optionsContainer(6);
+randomColors();
